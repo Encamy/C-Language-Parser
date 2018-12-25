508d65ee9d8829d8cbf92ad9a3e68392e69d32cf
Aleksei <alexxey67@gmail.com>
2018-12-25 20:38:54 +0300

    Add template C file

---
+++ src/example.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct Point {
+  int x;
+  int y;
+};
+
+bool isBST(struct node* node, int c) 
+{
+  return a > b;
+}
+
+int main(int argc, char * argv[]) 
+{
+  float a = -2.0;
+  bool variable;
+  int i = 0;
+  struct Point *point;
+  int someArray[25];
+    
+  point->x = 7;
+  for (i = 0; ; i++)
+  {
+	  someArray[i] = i * 2;
+	  if (i == 25)
+	  {
+		  break;
+	  }
+  }
+
+  for (i = 0; i < a; ++i) {
+    if (a + i > 0) {
+      c = false;
+    } else {
+      int z = 15;
+    }
+  }
+  someArray[0] = 13;
+  scanf("%d", &a);
+  printf("%d", a);
+  return 0;
+}
